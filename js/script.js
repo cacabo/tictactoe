3df7377a3cf2a9bc52065344620ecd51e6dde5d3
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-09 22:28:01 -0400

    Added complete button functionality and began updating UI

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ var board = [
 //  2 -> player 2's turn
 //  3 -> player 1 has won
 //  4 -> player 2 has won
-//  5 -> neither player has won
+//  5 -> draw
 var gameState = 0;
 
 //The following counters assign point values to validity of a route of winning
@@ -67,6 +67,9 @@ var resetGame = function() {
 //Returns 2 if player 2 has won
 //Returns 3 if neither player has won yet the game has ended (draw)
 var checkWon = function() {
+    //TODO
+
+
     for (i = 0; i < 8; i++) {
         if (p1_status[i] === 3) {
             gameState = 3;
@@ -84,17 +87,16 @@ var checkWon = function() {
     return 0;
 }
 
-var play = function() {
-    resetGame();
-    while (gameState !== 3 && gameState !== 4 && gameState !== 5) {
-        /*
-         * Wait for someone to click
-         */
-
-        checkWon();
-        gameState = 3;
-    }
-}
+// var play = function() {
+//     resetGame();
+//     while (gameState !== 3 && gameState !== 4 && gameState !== 5) {
+//         /*
+//          * Wait for someone to click
+//          */
+//
+//         checkWon();
+//     }
+// }
 
 var getBoard = function() {
     return board;
@@ -115,26 +117,174 @@ var choose = function(row, col) {
     }
     // If this location has not yet been chosen
     if ((gameState === 1 || gameState === 2) && board[row][col] === 0) {
+        if (gameState === 1)
+            gameState = 2;
+        else
+            gameState = 1;
         countMoves++;
         //Assign either a 1 or 2 to the board depending on whose turn it is
         board[row][col] = gameState;
+        checkWon();
     }
 }
+
+//Reset the board visually
+var resetBoard = function() {
+    $('.slot').removeClass('p1');
+    $('.slot').removeClass('p2');
+}
+
+
+//Add click event listeners to buttons and slots
+$(document).ready(function() {
+    $('#r0c0').click(function() {
+        if (gameState === 1 || gameState === 2) {
+            //Color the grid depending on who's turn it is
+            //If the board is not yet colored
+            if (board[0][0] === 0) {
+                if (gameState === 1)
+                    $(this).addClass('p1');
+                else
+                    $(this).addClass('p2');
+            }
+            //Perform backend action
+            choose(0, 0);
+        }
+    });
+
+    $('#r0c1').click(function() {
+        if (gameState === 1 || gameState === 2) {
+            //Color the grid depending on who's turn it is
+            //If the board is not yet colored
+            if (board[0][1] === 0) {
+                if (gameState === 1)
+                    $(this).addClass('p1');
+                else
+                    $(this).addClass('p2');
+            }
+            //Perform backend action
+            choose(0, 1);
+        }
+    });
+
+    $('#r0c2').click(function() {
+        if (gameState === 1 || gameState === 2) {
+            //Color the grid depending on who's turn it is
+            //If the board is not yet colored
+            if (board[0][2] === 0) {
+                if (gameState === 1)
+                    $(this).addClass('p1');
+                else
+                    $(this).addClass('p2');
+            }
+            //Perform backend action
+            choose(0, 2);
+        }
+    });
+
+    $('#r1c0').click(function() {
+        if (gameState === 1 || gameState === 2) {
+            //Color the grid depending on who's turn it is
+            //If the board is not yet colored
+            if (board[1][0] === 0) {
+                if (gameState === 1)
+                    $(this).addClass('p1');
+                else
+                    $(this).addClass('p2');
+            }
+            //Perform backend action
+            choose(1, 0);
+        }
+    });
+
+    $('#r1c1').click(function() {
+        if (gameState === 1 || gameState === 2) {
+            //Color the grid depending on who's turn it is
+            //If the board is not yet colored
+            if (board[1][1] === 0) {
+                if (gameState === 1)
+                    $(this).addClass('p1');
+                else
+                    $(this).addClass('p2');
+            }
+            //Perform backend action
+            choose(1, 1);
+        }
+    });
+
+    $('#r1c2').click(function() {
+        if (gameState === 1 || gameState === 2) {
+            //Color the grid depending on who's turn it is
+            //If the board is not yet colored
+            if (board[1][2] === 0) {
+                if (gameState === 1)
+                    $(this).addClass('p1');
+                else
+                    $(this).addClass('p2');
+            }
+            //Perform backend action
+            choose(1, 2);
+        }
+    });
+
+    $('#r2c0').click(function() {
+        if (gameState === 1 || gameState === 2) {
+            //Color the grid depending on who's turn it is
+            //If the board is not yet colored
+            if (board[2][0] === 0) {
+                if (gameState === 1)
+                    $(this).addClass('p1');
+                else
+                    $(this).addClass('p2');
+            }
+            //Perform backend action
+            choose(2, 0);
+        }
+    });
+
+    $('#r2c1').click(function() {
+        if (gameState === 1 || gameState === 2) {
+            //Color the grid depending on who's turn it is
+            //If the board is not yet colored
+            if (board[2][1] === 0) {
+                if (gameState === 1)
+                    $(this).addClass('p1');
+                else
+                    $(this).addClass('p2');
+            }
+            //Perform backend action
+            choose(2, 1);
+        }
+    });
+
+    $('#r2c2').click(function() {
+        if (gameState === 1 || gameState === 2) {
+            //Color the grid depending on who's turn it is
+            //If the board is not yet colored
+            if (board[2][2] === 0) {
+                if (gameState === 1)
+                    $(this).addClass('p1');
+                else
+                    $(this).addClass('p2');
+            }
+            //Perform backend action
+            choose(2, 2);
+        }
+    });
+
+    $('#reset').click(function() {
+        resetGame();
+        resetBoard();
+    });
+
+    $('#end').click(function() {
+        gameState = 5;
+    });
+});
+
 
 
 //When the page has finished loading, start playing the game
 $(document).ready(function() {
-    play();
-});
-
-$(document).ready(function() {
-    $('#r0c0').click(function() {
-        if (gameState === 1) {
-            choose(0, 0);
-            $(this).addClass('p1');
-        } else if (gameState === 2) {
-            choose(0, 0);
-            $(this).addClass('p2')
-        }
-    });
+    resetGame();
 });
