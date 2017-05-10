d4d656b8b5bdb17ac224025ff321e62aca344530
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-09 21:36:37 -0400

    Began modifying state of slots on the board

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
+/*
+ * FORMATTING
+ */
+
+$(document).ready(function() {
+    var w = $('.slot').width();
+    $('.slot').height(w);
+});
+
+/*
+ * PROGRAM
+ */
+
 //Initialize the game board (3x3 grid)
 var board = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0]
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
 ];
 
 //Game state
@@ -30,23 +43,23 @@ count_moves = 0;
 
 //Reset the game
 var resetGame = function() {
-  //Initialize the game board (3x3 grid)
-  board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-  ];
+    //Initialize the game board (3x3 grid)
+    board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
 
-  //Reset the game state
-  gameState = 0;
+    //Reset the game state
+    gameState = 0;
 
-  //Randomly assign either player 1 or player 2 to start
-  gameState = (Math.floor(Math.random() * 2)) + 1;
+    //Randomly assign either player 1 or player 2 to start
+    gameState = (Math.floor(Math.random() * 2)) + 1;
 
-  p1_status = [0, 0, 0, 0, 0, 0, 0, 0];
-  p2_status = [0, 0, 0, 0, 0, 0, 0, 0];
+    p1_status = [0, 0, 0, 0, 0, 0, 0, 0];
+    p2_status = [0, 0, 0, 0, 0, 0, 0, 0];
 
-  countMoves = 0;
+    countMoves = 0;
 }
 
 //Returns 0 if neither player has won
@@ -54,51 +67,74 @@ var resetGame = function() {
 //Returns 2 if player 2 has won
 //Returns 3 if neither player has won yet the game has ended (draw)
 var checkWon = function() {
-  for (i = 0; i < 8; i++) {
-    if (p1_status[i] === 3) {
-      gameState = 3;
-      return 1;
+    for (i = 0; i < 8; i++) {
+        if (p1_status[i] === 3) {
+            gameState = 3;
+            return 1;
+        } else if (p2_status[i] === 3) {
+            gameState = 4;
+            return 2;
+        }
     }
-    else if (p2_status[i] === 3) {
-      gameState = 4;
-      return 2;
+    if (countMoves === 9) {
+        gameState = 5;
+        return 3;
     }
-  }
-  if (countMoves === 9)
-    gameState = 5;
-    return 3;
-  //If no value has been returned yet, neither player has won
-  return 0;
+    //If no value has been returned yet, neither player has won
+    return 0;
 }
 
-function play() {
-  resetGame();
-  while (gameState !== 3 && gameState !== 4 && gameState !== 5) {
-    /*
-     * Wait for someone to click
-     */
+var play = function() {
+    resetGame();
+    while (gameState !== 3 && gameState !== 4 && gameState !== 5) {
+        /*
+         * Wait for someone to click
+         */
 
-    checkWon();
-    gameState = 3;
-  }
+        checkWon();
+        gameState = 3;
+    }
 }
 
 var getBoard = function() {
-  return board;
+    return board;
 }
 
 var getP1Status = function() {
-  return p1_status;
+    return p1_status;
 }
 
 var getP2Status = function() {
-  return p2_status;
+    return p2_status;
 }
 
 var choose = function(row, col) {
-  if (row > 3 || row < 0 || col > 3 || col < 0) {
-    console.log("ERROR: passed in invalid coords");
-    return;
-  }
-  // TODO
+    if (row > 3 || row < 0 || col > 3 || col < 0) {
+        console.log("ERROR: passed in invalid coords");
+        return;
+    }
+    // If this location has not yet been chosen
+    if ((gameState === 1 || gameState === 2) && board[row][col] === 0) {
+        countMoves++;
+        //Assign either a 1 or 2 to the board depending on whose turn it is
+        board[row][col] = gameState;
+    }
 }
+
+
+//When the page has finished loading, start playing the game
+$(document).ready(function() {
+    play();
+});
+
+$(document).ready(function() {
+    $('#r0c0').click(function() {
+        if (gameState === 1) {
+            choose(0, 0);
+            $(this).addClass('p1');
+        } else if (gameState === 2) {
+            choose(0, 0);
+            $(this).addClass('p2')
+        }
+    });
+});
